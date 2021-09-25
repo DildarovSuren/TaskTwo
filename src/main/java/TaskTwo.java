acf30f8e3aec241c2b520d0455a41ad4f902c4bf
Dildarov Suren <dildarovsuren@gmail.com>
2021-09-25 10:42:10 +0300

    v.2

--- src/main/java/TaskTwo.java
+++ src/main/java/TaskTwo.java
@@ -1,71 +1,86 @@
-import java.text.DecimalFormat;
-import java.util.Arrays;
 import java.util.Scanner;
 public class TaskTwo {
     public static void main(String[] args) {
-        /**
-         * Реализуем программу-калькулятор с простыми арифметичекими операциями
-         */
         Scanner in = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("##.####"); //Используем данный метод для форматирования ответа в консоли
-        System.out.println("Введите номер программы 1 - Калькулятор, 2 - Массивы");
-        int digit = in.nextInt();
-
-        if (digit == 1) {
-            System.out.println("Программа Калькулятор запущена");
-            System.out.println("Введите первое число:");
-            float a = in.nextFloat();
-            System.out.println("Введите второе число:");
-            float b = in.nextFloat();
-            System.out.println("Введите арифметическую операцию и нажмите ENTER:");
-            String c = in.next();
-
-            if(b == 0) {     //Объявляем условие, чтобы взять во внимание случай с делением на 0
-                switch (c) {
-                    case "/": System.out.println("Ошибка.На ноль делить нельзя");
-                        break;
-                    case "*": System.out.println(df.format(a * b));
-                        break;
-                    case "+": System.out.println(df.format(a + b));
-                        break;
-                    case "-": System.out.println(df.format(a - b));
-                        break;
-                    default: System.out.println("Введена некорректная арифметическая операция! Начните заново!");
-                        break;
-                }
-            } else {
-                switch (c) {
-                    case "/": System.out.printf("%.4f", (a / b));
-                        break;
-                    case "*": System.out.printf("%.4f", (a * b));
-                        break;
-                    case "+": System.out.printf("%.4f", (a + b));
-                        break;
-                    case "-": System.out.printf("%.4f", (a - b));
-                        break;
-                    default: System.out.println("Введена некорректная арифметическая операция! Начните заново!");
-                        break;
-                } }
-
-        } else if (digit == 2) { //Программа Масивы
-            System.out.println("Программа Массивы запущена");
-            System.out.println("Введите длину массива");
-            String[] wordsArr = new String [in.nextInt()];
-            System.out.println("Длина массива равна " + wordsArr.length);
-            System.out.println("Введите " + wordsArr.length + " слов через Enter");
-            String top = "";
-            for (int j = 0; j < wordsArr.length; j++) {
-                wordsArr[j] = in.next();
-            }
-            for (int k = 0; k < wordsArr.length; k++) {
-                if(wordsArr[k].length() >= top.length()){
-                    top = wordsArr[k];
-                }
-            }
-            System.out.println("Самое длинное слово в массиве: " + top);
-
+        System.out.println("Выберите номер программы (1 - калькулятор, 2 - массив): ");
+        int app = in.nextInt();
+        if (app == 1) {
+            calc();
+        } else if (app == 2) {
+            massive();
         } else {
-            System.out.println("Введен некорректный номер программы");
+            System.out.println("Ошибка. Введите корректный номер программы!");
+        }
+
+    }
+
+    public static void calc() { //Описание логики  калькулятора
+        Scanner in = new Scanner(System.in);
+        System.out.println("Программа Калькулятор запущена");
+        System.out.println("Введите первое число: ");
+        double first = in.nextDouble();
+        System.out.println("Введите действие: ");
+        String action = in.next();
+        System.out.println("Введите второе число: ");
+        double second = in.nextInt();
+
+        switch (action) {
+            case "/":
+                calcSlash(first, second);
+                break;
+            case "*":
+                calcX(first, second);
+                break;
+            case "+":
+                calcSum(first, second);
+                break;
+            case "-":
+                calcDel(first, second);
+                break;
+            default:
+                System.out.println("Введена некорректная арифметическая операция! Начните заново!");
+                break;
         }
     }
+
+    public static void calcSum(double first, double second) { // Сложение
+        System.out.printf("%.4f", (first + second));
+    }
+
+    public static void calcDel(double first, double second) { // Вычитание
+        System.out.printf("%.4f", (first - second));
+    }
+
+    public static void calcX(double first, double second) { // Умножение
+        System.out.printf("%.4f", (first * second));
+    }
+
+    public static void calcSlash(double first, double second) { // Деление
+        if (second == 0) {
+            System.out.println("Ошибка. На ноль делить нельзя");
+        } else {
+            System.out.printf("%.4f", (first / second));
+        }
+    }
+
+    public static void massive() { //Описание логики массивов
+        Scanner in = new Scanner(System.in);
+        System.out.println("Программа Массивы запущена");
+        System.out.println("Введите длину массива");
+        String[] wordsArr = new String[in.nextInt()];
+        System.out.println("Длина массива равна " + wordsArr.length);
+        System.out.println("Введите " + wordsArr.length + " слов через Enter");
+        String top = "";
+        for (int j = 0; j < wordsArr.length; j++) {
+            wordsArr[j] = in.next();
+        }
+        for (int k = 0; k < wordsArr.length; k++) {
+            if (wordsArr[k].length() >= top.length()) {
+                top = wordsArr[k];
+            }
+        }
+        System.out.println("Самое длинное слово в массиве: " + top);
+
+    }
 }
+
